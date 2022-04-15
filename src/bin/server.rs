6635cc2edbf9e2f78f7ee157160029d6d4309583
enigbe <ochekliyeenigbe@gmail.com>
2022-04-15 09:39:29 +0100

    feat(server): scaffold node server
    - create new actix application
    - add some empty handlers
    
    [WIP]

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,20 +1,48 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use lnnode::start_ldk;
 
-/// Start the LDK sample node
-async fn start() -> HttpResponse {
+/// Get help information on how to query the node
+async fn help(req: HttpRequest) -> HttpResponse {
+	// 1. Get path, i.e. "help"
+	let path = req.path().to_owned();
+	// 2. Find a way to pass this into the node's CLI
+	start_ldk().await;
+
+	HttpResponse::Ok().finish()
+}
+
+/// Get node information
+async fn nodeinfo() -> HttpResponse {
+	HttpResponse::Ok().finish()
+}
+
+/// List node peers
+async fn list_peers() -> HttpResponse {
+	HttpResponse::Ok().finish()
+}
+
+/// Connect another node to running node
+async fn connect_peer() -> HttpResponse {
 	HttpResponse::Ok().finish()
 }
 
 fn run() -> Result<Server, std::io::Error> {
-	let server = HttpServer::new(|| App::new().route("/", web::get().to(start)))
-		.bind("127.0.0.1:8000")?
-		.run();
+	let server = HttpServer::new(|| {
+		App::new()
+			.route("/nodeinfo", web::get().to(nodeinfo))
+			.route("/help", web::get().to(help))
+			.route("/connect_peer", web::get().to(connect_peer))
+			.route("/list_peers", web::get().to(list_peers))
+	})
+	.bind("127.0.0.1:8000")?
+	.run();
 
 	Ok(server)
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+	println!("Starting node server on port: 8000");
 	run()?.await
 }
